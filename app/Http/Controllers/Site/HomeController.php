c363823c367a12f3dcd917ef5bd3e1681cd1a6fb
marcelops1 <marcelops1@yahoo.com.br>
2015-11-12 08:53:55 -0200

    Criacao do controller home

---
+++ app/Http/Controllers/Site/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    public function getIndex()
+    {
+        return 'Home do Site';
+    }
+}
--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'SHbm1gZraaQWzvvl87blJY1JU0x4TrvS'),
 
     'cipher' => 'AES-256-CBC',
 
